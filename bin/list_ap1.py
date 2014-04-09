739c68e37e798612dd603c55a7e3344ba956e4d2
elenam <elenam@AlbireoPro.local>
2014-04-09 02:36:10 +0000

    list_ap, added no_file_notice, evening and dither sequences,  added test for this program.

--- bin/list_ap1.py
+++ bin/list_ap1.py
@@ -35,6 +35,9 @@ History 2013:
     to 4.1f (it was  5.2f). 
 
 01/09/2014 Added column with  normed flux for all three flats.  
+test: 
+./list_ap1.py -m 56721 -morning
+
 """
 
 import glob
@@ -56,48 +59,74 @@ import apogeeThar
 zone=20
 p0=apogeeThar.p0a.copy()
 
+morning="dddqqqtutudiiid"
+evening="dqtutud"
+dither='t'*17
+
+ditherA=0
+regex=""
+
 #------------------------
 def curSjd():
 # current mjd
   TAI_UTC =34; sjd1=(time.time() + TAI_UTC) / 86400.0 + 40587.3;  
   sjd= int (sjd1)
   return sjd
-#----------------------- 
-def getMorningSequence(mjd): 
-    pp="/data/apogee/utr_cdr/"
-    fNames="%s%s/apRaw-%s.fits"%(pp,mjd,"*")
-    files = glob.glob(fNames)
-    files=sorted(files)
-    nfiles=len(files)
-    map=''
-    if nfiles==0: 
-        print " - no files found -- " 
-        return None
-    else: 
-        for i,f in enumerate(sorted(files)):
-            hdr = pyfits.getheader(f)
-            imtype=hdr.get('IMAGETYP')
-            nreads=hdr.get('NFRAMES')
-            if imtype=="Dark": map=map+"d"
-            elif imtype=="QuartzFlat": map=map+"q"
-            elif imtype=="InternalFlat": map=map+"i"
-            elif imtype=="DomeFlat": map=map+"m"
-            elif imtype=="ArcLamp" and nreads==12: map=map+"t" # Thar
-            elif imtype=="ArcLamp" and nreads==40: map=map+"u" # Une
-            elif imtype=="Object" : map=map+"o" 
-            else:
-               print "wrong image type" 
-               return None 
-        regex="dddqqqtutudiiid"
-        m = re.search(regex, map)
-        if m==None:
-            print "  no morning sequence found, exit"
-            return None
-        else:
-          #  print m.start(), m.end()
-            return files[m.start():m.end()]
 
-# ......
+#-----
+def getFiles(mask):
+    files = glob.glob(mask)
+    return sorted(files)
+
+#----------------------- 
+def ifLamp(f, lamp ):   #  'LAMPUNE'; 'LAMPTHAR'
+    fexp=f[33:41]
+    fReduct="/data/apogee/quickred/?????/ap2D-a-%s.fits.fz" % (fexp)  
+    fileNames = glob.glob(fReduct)
+    if len(fileNames) > 1:  sys.exit(" -- error: several reduced arc files found -- ")
+    if len(fileNames) > 1:  sys.exit(" -- error: no reduced arc file found -- ")
+    lamp=pyfits.getval(fileNames[0],lamp,0)
+    if lamp == 1:  return True
+    else: return False
+
+#-------------  
+def getMap(files):
+    if len(files)==0:  return None
+    files=sorted(files)
+    map=''    
+    for i,f in enumerate(files):
+        if not os.path.isfile(f):
+#            print "%3i    no file: %s " % (i,f)
+            map=map+"-"
+            continue
+        hdr = pyfits.getheader(f)
+        imtype=hdr.get('IMAGETYP')
+        nreads=hdr.get('NFRAMES')
+        if imtype=="Dark": map=map+"d"
+        elif imtype=="QuartzFlat": map=map+"q"
+        elif imtype=="InternalFlat": map=map+"i"
+        elif imtype=="DomeFlat": map=map+"m"
+        elif imtype=="ArcLamp" and ifLamp(f, 'LAMPTHAR'):  
+               map=map+"t" # Thar
+        elif imtype=="ArcLamp" and ifLamp(f, 'LAMPUNE'):  
+               map=map+"u" # Une
+        elif imtype=="Object" : 
+               map=map+"o" 
+        else:
+            print "wrong image type, f=%s, imtype = %s" % (f,imtype) 
+            map=map+"?"
+      #  print i, f, map
+    return map             
+
+# ----
+def getSequence(map, regex): 
+    m = re.search(regex, map)
+    if m==None:
+        return None
+    else:
+        return  [m.start(), m.end()]
+
+# ------
 def getOffset(qrfile1):
     try :
         data1 = pyfits.getdata(qrfile1,0)
@@ -145,12 +174,13 @@ def getFlux(f):
 
                     
 #...
+
 def  list_one_file(i,f,mjd):
     path="/data/apogee/quickred/%s" % mjd
     fexp=f[33:41]
     qrfile1="%s/ap1D-a-%s.fits.fz" % (path,fexp)
     qrfile2="%s/ap2D-a-%s.fits.fz" % (path,fexp)
-
+    
     ff=f
     if os.path.exists(qrfile2):  
         ff=qrfile2
@@ -172,10 +202,15 @@ def  list_one_file(i,f,mjd):
     if plate==None: plate="----"
 
     dth= float(hdr['DITHPIX'])
-    if dth==12.994: sdth="A-%4.1f" % dth
-    elif dth==13.499: sdth="B-%4.1f" % dth
-    else: sdth="?-%4.1f"% dth    
-
+    if dth==12.994: sdth="A  %4.1f" % dth
+    elif dth==13.499: sdth="B  %4.1f" % dth
+    else: sdth="?  %4.1f"% dth 
+    if (regex==dither):
+         global ditherA
+         if i==0: 
+             ditherA=dth
+         diffDither= dth - ditherA
+        
     imtype= hdr.get('IMAGETYP')
     offset=" - "
     if imtype=="ArcLamp":
@@ -185,7 +220,7 @@ def  list_one_file(i,f,mjd):
           imtype=imtype+"-Thar"
           offs=getOffset(qrfile1)
           if offs != None: 
-              offset=offs
+              offset=offs              
       else: imtype=imtype+"----"
     if imtype=="QuartzFlat":  imtype="QuarFlat"
     if imtype=="InternalFlat": imtype="InteFlat"    
@@ -206,7 +241,7 @@ def  list_one_file(i,f,mjd):
 #            std=getStd(f)
     
 # print information
-    ss1="%3i "% (i+1)  #i
+    ss1="%s %3i "% (mjd, i+1)  #i
     ss1=ss1+"%s  " % (hdr['DATE-OBS'][11:16]) # UT time
     ss1=ss1+"%s " % (f[33:41])  # exp number
     ss1=ss1+"%s" % (imtype)  # image type
@@ -220,68 +255,99 @@ def  list_one_file(i,f,mjd):
     comm=hdr["OBSCMNT"]
     if comm  !="None": 
         ss1=ss1+ " %s" % comm[0:8] # comment
+    if (regex==dither):
+        ss1=ss1+" %5.2f" % diffDither
+    
     print ss1 #, len(ss1)
 
-#...
+
+def  list_one_mjd(mjd):
+#    bs=os.path.basename(sys.argv[0])
+#    bs=os.path.abspath(sys.argv[0])
+#    print  "# %s -m1 %s" % (bs, mjd)
+        
+#    print "APOGEE data list,   mjd=%s" % mjd    
+    pp="/data/apogee/utr_cdr/"
+    fNames="%s%s/apRaw-%s.fits"%(pp,mjd,"*")   # raw data
+#    print "   raw_data: ", fNames
+    fReduct="/data/apogee/quickred/%s/ap2D-a-*.fits.fz" % (mjd)  # quick reduction
+#    print "   quick_red:", fReduct
+    fArch="/data/apogee/archive/%s/apR-[a,b,c]-*.apz" % (mjd)  # archive
+#    print "   archive:  ", fArch
+       
+    files=getFiles(fNames)
+    if len(files)==0: 
+        print " - mjd=%s, no files found  -- " % (mjd)        
+        #sys.exit(" - mjd=%s, no files found  -- " % (mjd))
+        return 
+
+    files=sorted(files)
+    lastNumber=files[-1][33:41]
+    dd=int(round(int(lastNumber)/10000)*10000)
+    s2=int(lastNumber)-dd
+
+    files1=[]
+    for i in range(1,s2+1):
+        fItem="%s%s/apRaw-%8i.fits"%(pp,mjd,dd+i)   # raw data 
+        files1.append(fItem)
+
+    files=files1
+
+    if args.morning or args.evening or args.dither:
+        map=getMap(files)
+        if args.morning: regex=morning
+        elif args.evening:  regex=evening
+        elif args.dither:  regex=dither; 
+        else:   sys.exit("  error: 1")   
+        mapRange=getSequence(map, regex)
+        if mapRange==None:
+            print  "  no sequence found, exit"
+            return              
+    #    print mapRange[0], mapRange[1]
+        files=files[mapRange[0]:mapRange[1]]
+    #    print len(files)    
+                    
+    for i,f in enumerate(sorted(files)):
+        if not os.path.isfile(f):
+            ss1="%s %3i"% (mjd, i+1)  #i
+            print "%s      no file found:  %s " % (ss1, f)
+            continue
+        list_one_file(i,f, mjd)   
+#--------------
 
 if __name__ == "__main__":
 
     sjd=curSjd()
     desc = 'list of files for one night of apogee observations'
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('-m', '--mjd', 
-           help='enter mjd, default is current mjd',    
+    parser.add_argument('-m', '--mjd',  help='enter mjd, default is current mjd',    
            default=int(sjd), type=int)
-
+    parser.add_argument('-m2', '--mjd2',  help='and mjd if use range of dates, default is mjd', type=int)           
     parser.add_argument('-morning', '--morning', action="store_true") 
-           
+    parser.add_argument('-evening', '--evening', action="store_true") 
+    parser.add_argument('-dither', '--dither', action="store_true")            
     args = parser.parse_args()    
     mjd=args.mjd
+    if args.mjd2==None: mjd2=mjd
+    else: mjd2=args.mjd2
+    if mjd2 < mjd:
+        dd=mjd; mjd=mjd2; mjd2=dd
     
-#    bs=os.path.basename(sys.argv[0])
-#    bs=os.path.abspath(sys.argv[0])
-#    print  "# %s -m1 %s" % (bs, mjd)
-        
-    print "APOGEE data list,   mjd=%s" % mjd    
-    pp="/data/apogee/utr_cdr/"
-    fNames="%s%s/apRaw-%s.fits"%(pp,mjd,"*")
-    print "   raw_data: ", fNames
-    ppqr="/data/apogee/quickred/%s/ap2D-a-*.fits.fz" % (mjd)
-    print "   quick_red:", ppqr
-    pparc="/data/apogee/archive/%s/apR-[a,b,c]-*.apz" % (mjd)
-    print "   archive:  ", pparc
     
-    files = glob.glob(fNames)
-    files = sorted(files)
-    nfiles=len(files)
-    if nfiles==0: 
-        print " - no files found -- " 
-        sys.exit(0)
-
-    if args.morning:
-        files=getMorningSequence(mjd)        
-        # add check result
-        if files == None: 
-             sys.exit(0)
-        nfiles=len(files)
-    
-    line="-"*80
-    print line
-    prc="%"
+    line="-"*80;  prc="%"
 #    header=" i   UT   File/Exp   Imtype  Nread  Dth  Ct-Plate Arch Offset %sFlux Std Comm" % prc
-    header=" i   UT   File/Exp   Imtype  Nread  Dth  Ct-Plate Arch Offset %sFlux Comm" % prc
-    print header  
-    print line 
-    nfiles=len(files)
-    if nfiles==0: 
-        print " - no files found -- " 
-    else: 
-        for i,f in enumerate(sorted(files)):
-         #    if (i)/15.0 == int((i)/15) and i!=0:
-         #         print line, "\n", header, "\n",line 
-             list_one_file(i,f, mjd) 
-                  
-    print line    
- #   ss="    THAR offset=X0-%5.2f, pix " % (p0[0][1])
- #   print ss, "\n"
-#...
+    header="mjd     i   UT   File/Exp  Imtype Nread A/B Dth  Ct-Plate Arch Offset %sFlux Comm" % prc
+
+    for m in range(mjd, mjd2+1):
+        pp="/data/apogee/utr_cdr/"
+        fNames="%s%s/apRaw-%s.fits"%(pp,m,"*")   # raw data
+        print "APOGEE data list,   mjd=%s" % m  
+        print "raw_data: ", fNames  
+        print line, "\n",  header, "\n", line        
+        files=getFiles(fNames)
+        if len(files)==0: 
+             print " - no files found  for this mjd  %s-- " % m 
+             print line, "\n" 
+             continue
+        list_one_mjd(m)
+        print line, "\n" 
