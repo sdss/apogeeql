3d0b6569556f6828e28b18c915035de6cfccd7a2
David Nidever <dnidever@montana.edu>
2021-06-09 08:55:52 -0600

    Making apogeeql pip installable

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-import distutils
-from distutils.core import setup, Extension
+#!/usr/bin/env python
 
-import sdss3tools
-import os
+from setuptools import setup, find_packages
 
-sdss3tools.setup(
-    description = "APOGEE Quick Look Actor",
-    )
-
+setup(name='apogeeql',
+      version='2.0.0',
+      description='APOGEE quicklook actor',
+      author='Stephane Beland, David Nidever',
+      author_email='dnidever@montana.edu',
+      url='https://github.com/sdss/apogeeql',
+      packages=find_packages(exclude=["tests"]),
+      scripts=['bin/apogeeql','bin/apogeeql','bin/runQuickLook.py'],
+      install_requires=['numpy','astropy(>=4.0)','scipy','apogee_mountain'])
