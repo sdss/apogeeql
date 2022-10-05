871d1f156854eb5e39022d556e169c86b7cdf7a7
johndonor3 <j.donor@tcu.edu>
2022-10-05 13:28:50 -0600

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python
+# encoding: utf-8
+#
+# setup.py
+#
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='apogeeql',
-      version='2.0.0',
-      description='APOGEE quicklook actor',
-      author='Stephane Beland, David Nidever',
-      author_email='dnidever@montana.edu',
-      url='https://github.com/sdss/apogeeql',
-      packages=find_packages(exclude=["tests"]),
-      scripts=['bin/apogeeql','bin/apogeeql','bin/runQuickLook.py'],
-      install_requires=['numpy','astropy(>=4.0)','scipy','apogee_mountain'])
+setup()
